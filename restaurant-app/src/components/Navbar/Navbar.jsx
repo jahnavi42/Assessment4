d1c0fd9f4ddf42fa9892de64ca8e708312075bbd
M.Jahnavi <you@example.com>
2022-09-18 11:20:46 +0530

    abc

--- restaurant-app/src/components/Navbar/Navbar.jsx
+++ restaurant-app/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {MdOutlineRestaurantMenu} from 'react-icons/md';
+import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className="app_navBar">
+    <div></div>
+    
+  </nav>
 );
 
 export default Navbar;
